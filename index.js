78e8ca1f4ceb83ad3e4b2c0ce18615d968a4ef31
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-09-15 22:19:49 +0200

    added function onHeart

--- index.js
+++ index.js
@@ -11,11 +11,19 @@ function initRating() {
 
 }
 
-
+const iconSvg = document.querySelector('.icon-heart-svg')
 const heartIconElem = document.querySelector('.heart-icon-elem')
 console.log(heartIconElem);
-heartIconElem.addEventListener('checked', onHeart)
+heartIconElem.addEventListener('change', onHeart)
 
-function onHeart(e) {
-    console.dir(e);
+function onHeart(e) {console.log(e);
+    if (e.currentTarget.checked) {
+        console.log(e.currentTarget.checked);
+        iconSvg.classList.add('svg-active');
+        localStorage.setItem('saveCheckedFavorite', e.target._id);
+    } else {
+          iconSvg.classList.remove('svg-active');
+        localStorage.removeItem('saveCheckedFavorite', e.target._id);
+    }
+    
 }
